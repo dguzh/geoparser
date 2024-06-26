31330fbc547085911a36b5dce0344a9398e08db4
nicolasspring <springn.ch@gmail.com>
2024-06-26 19:13:44 +0200

    define methods for conditionals once

--- geoparser/gazetteer.py
+++ geoparser/gazetteer.py
@@ -37,45 +37,45 @@ class Gazetteer(ABC):
             location, self.location_description_template
         )
 
-    @staticmethod
-    def format_location_description(
-        location: dict[str, t.Union[int, str, float]], template: str
+    def evaluate_conditionals(
+        self,
+        cond_expr: str,
+    ) -> tuple[t.Optional[str], t.Optional[t.Callable[[dict], bool]]]:
+        match = re.match(r"COND\[(.+?), (any|all)\{(.+?)\}\]", cond_expr)
+        if not match:
+            return None, None
+
+        text, condition_type, keys = match.groups()
+        keys = [key.strip("<> ") for key in keys.split(",")]
+
+        if condition_type == "any":
+            return text, lambda loc: any(loc.get(key) for key in keys)
+        elif condition_type == "all":
+            return text, lambda loc: all(loc.get(key) for key in keys)
+        return None, None
+
+    def substitute_conditionals(
+        self, location: dict[str, t.Union[int, str, float]], template: str
     ) -> str:
+        conditionals = re.findall(r"COND\[.+?\]", template)
+        for cond in conditionals:
+            text, conditional_func = self.evaluate_conditionals(cond)
+            if conditional_func:
+                replacement_text = text if conditional_func(location) else ""
+                template = template.replace(cond, replacement_text, 1)
+        return template
+
+    def format_location_description(
+        self, location: dict[str, t.Union[int, str, float]], template: str
+    ) -> str:
+
+        def substitute_keys(match: re.Match) -> str:
+            key = match.group("key")
+            value = location.get(key)
+            return f"{match.group('pre')}{value}{match.group('post')}" if value else ""
+
         if location:
-
-            def evaluate_conditionals(
-                cond_expr: str,
-            ) -> tuple[t.Optional[str], t.Optional[t.Callable[[dict], bool]]]:
-                match = re.match(r"COND\[(.+?), (any|all)\{(.+?)\}\]", cond_expr)
-                if not match:
-                    return None, None
-
-                text, condition_type, keys = match.groups()
-                keys = [key.strip("<> ") for key in keys.split(",")]
-
-                if condition_type == "any":
-                    return text, lambda loc: any(loc.get(key) for key in keys)
-                elif condition_type == "all":
-                    return text, lambda loc: all(loc.get(key) for key in keys)
-                return None, None
-
-            def substitute_conditionals(template: str) -> str:
-                conditionals = re.findall(r"COND\[.+?\]", template)
-                for cond in conditionals:
-                    text, conditional_func = evaluate_conditionals(cond)
-                    if conditional_func:
-                        replacement_text = text if conditional_func(location) else ""
-                        template = template.replace(cond, replacement_text, 1)
-                return template
-
-            def substitute_keys(match: re.Match) -> str:
-                key = match.group("key")
-                value = location.get(key)
-                return (
-                    f"{match.group('pre')}{value}{match.group('post')}" if value else ""
-                )
-
-            template = substitute_conditionals(template)
+            template = self.substitute_conditionals(location, template)
             formatted_text = re.sub(
                 r"(?P<pre>[^\s<>]*?)<(?P<key>\w+)>(?P<post>[^\s<>]*)",
                 substitute_keys,
