fcd921aace489f4997edaec8ad22d9a7068400a9
nicolasspring <springn.ch@gmail.com>
2024-06-30 20:17:53 +0200

    add pydantic models for config

---
+++ geoparser/config/models.py
@@ -0,0 +1,28 @@
+import typing as t
+
+from pydantic import BaseModel
+
+
+class Column(BaseModel):
+    name: str
+    type: str
+    primary: bool = False
+
+
+class VirtualTable(BaseModel):
+    name: str
+    using: t.Optional[str] = None
+    args: list[str]
+    kwargs: dict[str, str]
+
+
+class GazetteerData(BaseModel):
+    name: str
+    url: str
+    columns: list[Column]
+    virtual_tables: list[VirtualTable] = []
+
+
+class GazetteerConfig(BaseModel):
+    name: str
+    data: list[GazetteerData]
