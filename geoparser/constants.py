1cf43016608adf7c26d9409ed09a6cda7efc9972
nicolasspring <springn.ch@gmail.com>
2024-06-30 20:20:13 +0200

    read gazetteer config into constants

--- geoparser/constants.py
+++ geoparser/constants.py
@@ -1,5 +1,7 @@
+from geoparser.config import get_gazetteer_configs
 from geoparser.geonames import GeoNames
 
 GAZETTEERS = {"geonames": GeoNames}
+GAZETTEERS_CONFIG = get_gazetteer_configs()
 MAX_ERROR = 20039  # half Earth's circumference in km
 MODES = {"download": "download"}
