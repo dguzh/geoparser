898caad948bbcf66501852eeafe9c4c4eed1b440
nicolasspring <springn.ch@gmail.com>
2024-07-25 22:18:32 +0200

    use same corpus for both tests

--- geoparser/tests/test_trainer.py
+++ geoparser/tests/test_trainer.py
@@ -10,7 +10,12 @@ def corpus_good_annotations() -> list[tuple[str, list[tuple[str, int, int, int]]
         (
             "Roc Meler is mentioned on Radio Andorra.",
             [("Roc Meler", 0, 9, 2994701), ("Radio Andorra", 26, 39, 3039328)],
-        )
+        ),
+        (
+            "Typhoon hit Taiwan today #prayfortaiwan",
+            [("Taiwan", 12, 18, 3039328), ("taiwan", 33, 39, 3039328)],
+        ),
+        ("Some End of Sentence|New York!!!", [("New York", 21, 29, 3039328)]),
     ]
     return corpus
 
@@ -21,7 +26,12 @@ def corpus_bad_annotations() -> list[tuple[str, list[tuple[str, int, int, int]]]
         (
             "Roc Meler is mentioned on Radio Andorra.",
             [("Roc Meler", 0, 7, 2994701), ("Radio Andorra", 23, 39, 3039328)],
-        )
+        ),
+        (
+            "Typhoon hit Taiwan today #prayfortaiwan",
+            [("Taiwan", 11, 18, 3039328), ("taiwan", 33, 40, 3039328)],
+        ),
+        ("Some End of Sentence|New York!!!", [("New York", 0, 30, 3039328)]),
     ]
     return corpus
 
@@ -31,7 +41,7 @@ def geodocs_corpus(
     trainer_real_data: GeoparserTrainer,
     corpus_good_annotations: list[tuple[str, list[tuple[str, int, int, int]]]],
 ) -> list[GeoDoc]:
-    return [trainer_real_data.nlp(corpus_good_annotations[0][0])]
+    return [trainer_real_data.nlp(seg[0]) for seg in corpus_good_annotations]
 
 
 def test_find_toponym(
@@ -40,8 +50,8 @@ def test_find_toponym(
     corpus_bad_annotations: list[tuple[str, list[tuple[str, int, int, int]]]],
     geodocs_corpus: list[GeoDoc],
 ):
-    for good_segment, bad_segment in zip(
-        corpus_good_annotations, corpus_bad_annotations
+    for i, (good_segment, bad_segment) in enumerate(
+        zip(corpus_good_annotations, corpus_bad_annotations)
     ):
         for good_annot, bad_annot in zip(
             [annot for annot in good_segment[1]], [annot for annot in bad_segment[1]]
@@ -50,63 +60,46 @@ def test_find_toponym(
             bad_start, bad_end = bad_annot[1], bad_annot[2]
             toponym = good_annot[0]
             assert trainer_real_data.find_toponym(
-                toponym, geodocs_corpus[0], bad_start, bad_end
+                toponym, geodocs_corpus[i], bad_start, bad_end
             ) == (good_start, good_end)
 
 
-@pytest.mark.parametrize(
-    "text,annotated_span",
-    [
-        ("Typhoon hit Taiwan today #prayfortaiwan", (33, 39)),
-        ("Some End of Sentence|New York!!!", (21, 29)),
-    ],
-)
 def test_retokenize_toponym(
-    trainer_real_data: GeoparserTrainer, text: str, annotated_span: tuple[int, int]
+    trainer_real_data: GeoparserTrainer,
+    corpus_good_annotations: list[tuple[str, list[tuple[str, int, int, int]]]],
+    geodocs_corpus: list[GeoDoc],
 ):
-    doc = trainer_real_data.nlp(text)
-    annotated_toponym = doc.text[slice(*annotated_span)]
-    len_before = len(doc)
-    # no span can be created because annotation is not at token boundaries
-    assert doc.char_span(*annotated_span) is None
-    # annotated toponym (or parts of it if whitespace-delimited) is not a token
-    assert not all(
-        toponym in [token.text for token in doc]
-        for toponym in annotated_toponym.split()
-    )
-    trainer_real_data.retokenize_toponym(doc, *annotated_span)
-    len_after = len(doc)
-    span = doc.char_span(*annotated_span)
-    # after retokenization, we can create a span and we have more tokens than before
-    assert span is not None
-    assert span.text == annotated_toponym
-    assert len_before < len_after
-    # annotated toponym (or parts of it if whitespace-delimited) are tokens
-    for toponym in annotated_toponym.split():
-        assert toponym in [token.text for token in doc]
-
-
-def test_retokenize_toponym_do_nothing(trainer_real_data: GeoparserTrainer):
-    """
-    method has no side-effects on good segments
-    """
-    text = "Typhoon hit Taiwan today"
-    doc = trainer_real_data.nlp(text)
-    annotated_span = (12, 18)
-    annotated_toponym = doc.text[slice(*annotated_span)]
-    len_before = len(doc)
-    tokens_before = [token.text for token in doc]
-    span = doc.char_span(*annotated_span)
-    # span can be found before
-    assert span is not None
-    assert span.text == annotated_toponym
-    trainer_real_data.retokenize_toponym(doc, *annotated_span)
-    len_after = len(doc)
-    tokens_after = [token.text for token in doc]
-    span = doc.char_span(*annotated_span)
-    # same span can be found after
-    assert span is not None
-    assert span.text == annotated_toponym
-    # number and text of tokens is still the same
-    assert len_before == len_after
-    assert tokens_before == tokens_after
+    for segment, doc in zip(corpus_good_annotations, geodocs_corpus):
+        for toponym in segment[1]:
+            annotated_span = toponym[1:3]
+            annotated_toponym = doc.text[slice(*annotated_span)]
+            len_before = len(doc)
+            tokens_before = [token.text for token in doc]
+            span = doc.char_span(*annotated_span)
+            # for good segments, the method must not have any side-effects
+            good_segment = span is not None
+            # in good segments, we can find the span from the beginning
+            if good_segment:
+                assert span.text == annotated_toponym
+            # in bad segments, the annotated toponym (or parts of it if whitespace-delimited)
+            # is not a token
+            else:
+                assert not all(
+                    toponym in [token.text for token in doc]
+                    for toponym in annotated_toponym.split()
+                )
+            trainer_real_data.retokenize_toponym(doc, *annotated_span)
+            len_after = len(doc)
+            tokens_after = [token.text for token in doc]
+            span = doc.char_span(*annotated_span)
+            assert span.text == annotated_toponym
+            # number and text of tokens is still the same
+            if good_segment:
+                assert len_before == len_after
+                assert tokens_before == tokens_after
+            # we have more tokens overall and the annotated toponym
+            # (or parts of it if whitespace-delimited) is a token
+            else:
+                assert len_before < len_after
+                for toponym in annotated_toponym.split():
+                    assert toponym in [token.text for token in doc]
