77a4e1471ee562a08fa8b657cd692269e072c7d3
nicolasspring <springn.ch@gmail.com>
2024-10-12 18:13:20 +0200

    add tests for geonames location description

--- geoparser/tests/test_geonames.py
+++ geoparser/tests/test_geonames.py
@@ -28,3 +28,119 @@ def test_read_file(geonames: GeoNames, test_chunk_full: pd.DataFrame):
     file_content = list(file_content)
     assert len(file_content) == n_chunks
     assert file_content[0].equals(test_chunk_full)
+
+
+@pytest.mark.parametrize(
+    "location,expected",
+    [
+        (  # base case
+            {
+                "name": "Uster",
+                "feature_type": "city",
+                "admin2_name": "Uster",
+                "admin1_name": "Zürich",
+                "country_name": "Switzerland",
+            },
+            "Uster (city) in Uster, Zürich, Switzerland",
+        ),
+        (  # country can be missing
+            {
+                "name": "Uster",
+                "feature_type": "city",
+                "admin2_name": "Uster",
+                "admin1_name": "Zürich",
+            },
+            "Uster (city) in Uster, Zürich",
+        ),
+        (  # admin1 can be missing
+            {
+                "name": "Uster",
+                "feature_type": "city",
+                "admin2_name": "Uster",
+                "country_name": "Switzerland",
+            },
+            "Uster (city) in Uster, Switzerland",
+        ),
+        (  # admin2 can be missing
+            {
+                "name": "Uster",
+                "feature_type": "city",
+                "admin1_name": "Zürich",
+                "country_name": "Switzerland",
+            },
+            "Uster (city) in Zürich, Switzerland",
+        ),
+    ],
+)
+def test_create_location_description_base(
+    geonames: GeoNames, location: dict, expected: str
+):
+    actual = geonames.create_location_description(location)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "location,expected",
+    [
+        (  # countries are not part of any further administrative divisions
+            {
+                "name": "Switzerland",
+                "feature_type": "independent political entity",
+                "country_name": "Switzerland",
+            },
+            "Switzerland (independent political entity)",
+        ),
+        (  # description for countries will not include admin1 and admin2 even if part of location
+            {
+                "name": "Switzerland",
+                "feature_type": "independent political entity",
+                "admin2_name": "asdf",
+                "admin1_name": "asdf",
+                "country_name": "Switzerland",
+            },
+            "Switzerland (independent political entity)",
+        ),
+        (  # first-order admin divisions are only part of a country
+            {
+                "name": "Zürich",
+                "feature_type": "first-order administrative division",
+                "country_name": "Switzerland",
+            },
+            "Zürich (first-order administrative division) in Switzerland",
+        ),
+        (  # description for first-order admin divisions will not include admin1 and admin2 even if part of location
+            {
+                "name": "Zürich",
+                "feature_type": "first-order administrative division",
+                "admin2_name": "asdf",
+                "admin1_name": "Zürich",
+                "country_name": "Switzerland",
+            },
+            "Zürich (first-order administrative division) in Switzerland",
+        ),
+        (  # second-order admin divisions are only part of a country and a first-order admin division
+            {
+                "name": "Uster",
+                "feature_type": "second-order administrative division",
+                "admin1_name": "Zürich",
+                "country_name": "Switzerland",
+            },
+            "Uster (second-order administrative division) in Zürich, Switzerland",
+        ),
+        (  # description for second-order admin divisions will not include admin1 and admin2 even if part of location
+            {
+                "name": "Uster",
+                "feature_type": "second-order administrative division",
+                "admin2_name": "asdf",
+                "admin1_name": "Zürich",
+                "country_name": "Switzerland",
+            },
+            "Uster (second-order administrative division) in Zürich, Switzerland",
+        ),
+    ],
+)
+def test_create_location_description_divisions(
+    geonames: GeoNames, location: dict, expected: str
+):
+    actual = geonames.create_location_description(location)
+    assert actual == expected
