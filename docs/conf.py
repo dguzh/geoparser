f630037aff76bc70d159558966b5c6a0608007f1
diego.gomes <diego.gomes@uzh.ch>
2024-11-06 16:27:39 +0100

    run black formatter

--- docs/conf.py
+++ docs/conf.py
@@ -6,29 +6,29 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../geoparser'))
+sys.path.insert(0, os.path.abspath("../geoparser"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'geoparser'
-copyright = '2024, Diego Gomes'
-author = 'Diego Gomes'
-release = '0.1.8'
+project = "geoparser"
+copyright = "2024, Diego Gomes"
+author = "Diego Gomes"
+release = "0.1.8"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
