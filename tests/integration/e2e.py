7546f6501b93a9f0ac030f3862ffa8fd4d6ef3ce
nicolasspring <springn.ch@gmail.com>
2024-11-24 11:04:17 +0100

    run script with respective gazetteer

--- tests/integration/e2e.py
+++ tests/integration/e2e.py
@@ -1,81 +1,77 @@
-#########################################################
-# GeoNames
-#########################################################
-
-
 from geoparser import Geoparser
-
-geo = Geoparser(
-    spacy_model="en_core_web_sm",
-    transformer_model="dguzh/geo-all-MiniLM-L6-v2",
-    gazetteer="geonames",
-)
-
-texts = [
-    "Zurich is a city rich in history.",
-    "Geneva is known for its role in international diplomacy.",
-    "Munich is famous for its annual Oktoberfest celebration.",
-]
-
-docs = geo.parse(texts)
-
-for doc in docs:
-    identifiers = [(l["name"], l["admin1_name"], l["country_name"]) for l in doc.locations]
-    for toponym, identifier in zip(doc.toponyms, identifiers):
-        print(toponym, "->", identifier)
-
-#########################################################
-# Trainer
-#########################################################
-
 from geoparser import GeoparserTrainer
 
-train_corpus = [
-    {
-        "text": "Zurich is a city in Switzerland.",
-        "toponyms": [
-            {"text": "Zurich", "start": 0, "end": 6, "loc_id": "2657896"},
-            {"text": "Switzerland", "start": 20, "end": 31, "loc_id": "2658434"}
+import typer
+
+
+def run_gazetteer(gazetteer: str):
+    print("----------\nrun_gazetteer\n----------")
+    geo = Geoparser(
+        spacy_model="en_core_web_sm",
+        transformer_model="dguzh/geo-all-MiniLM-L6-v2",
+        gazetteer=gazetteer,
+    )
+    texts = [
+        "Zurich is a city rich in history.",
+        "Geneva is known for its role in international diplomacy.",
+        "Munich is famous for its annual Oktoberfest celebration.",
+    ]
+    docs = geo.parse(texts)
+    for doc in docs:
+        identifiers = [
+            (l["name"], l["admin1_name"], l["country_name"]) for l in doc.locations
         ]
-    },
-    {
-        "text": "Geneva is known for international diplomacy.",
-        "toponyms": [
-            {"text": "Geneva", "start": 0, "end": 6, "loc_id": "2660646"}
-        ]
-    },
-    {
-        "text": "Munich hosts the annual Oktoberfest.",
-        "toponyms": [
-            {"text": "Munich", "start": 0, "end": 6, "loc_id": "2867714"}
-        ]
-    }
-]
+        for toponym, identifier in zip(doc.toponyms, identifiers):
+            print(toponym, "->", identifier)
 
-trainer = GeoparserTrainer(
-    spacy_model="en_core_web_sm", transformer_model="bert-base-uncased"
-)
 
-train_docs = trainer.annotate(train_corpus)
+def run_trainer(gazetteer: str):
+    print("----------\run_trainer\n----------")
+    train_corpus = [
+        {
+            "text": "Zurich is a city in Switzerland.",
+            "toponyms": [
+                {"text": "Zurich", "start": 0, "end": 6, "loc_id": "2657896"},
+                {"text": "Switzerland", "start": 20, "end": 31, "loc_id": "2658434"},
+            ],
+        },
+        {
+            "text": "Geneva is known for international diplomacy.",
+            "toponyms": [{"text": "Geneva", "start": 0, "end": 6, "loc_id": "2660646"}],
+        },
+        {
+            "text": "Munich hosts the annual Oktoberfest.",
+            "toponyms": [{"text": "Munich", "start": 0, "end": 6, "loc_id": "2867714"}],
+        },
+    ]
+    trainer = GeoparserTrainer(
+        spacy_model="en_core_web_sm",
+        transformer_model="bert-base-uncased",
+        gazetteer=gazetteer,
+    )
+    train_docs = trainer.annotate(train_corpus)
+    output_path = "path_to_custom_model"
+    trainer.train(train_docs, output_path=output_path)
+    test_docs = trainer.annotate(train_corpus)
+    trainer.resolve(test_docs)
+    evaluation_results = trainer.evaluate(test_docs)
+    print(evaluation_results)
 
-output_path = "path_to_custom_model"
 
-trainer.train(train_docs, output_path=output_path)
+def use_model(gazetteer: str):
+    print("----------\use_model\n----------")
+    geo = Geoparser(
+        spacy_model="en_core_web_sm", transformer_model="path_to_custom_model"
+    )
+    docs = geo.parse(["New text to parse"])
+    print(docs)
 
-test_docs = trainer.annotate(train_corpus)
 
-trainer.resolve(test_docs)
+def main(gazetteer: str):
+    run_gazetteer(gazetteer)
+    run_trainer(gazetteer)
+    use_model(gazetteer)
 
-evaluation_results = trainer.evaluate(test_docs)
 
-print(evaluation_results)
-
-#########################################################
-# Use model
-#########################################################
-
-from geoparser import Geoparser
-
-geo = Geoparser(spacy_model="en_core_web_sm", transformer_model="path_to_custom_model")
-
-docs = geo.parse(["New text to parse"])
+if __name__ == "__main__":
+    typer.run(main)
