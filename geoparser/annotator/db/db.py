309c8841139640fe26c5cac6880a62ce0bb01406
nicolasspring <springn.ch@gmail.com>
2025-01-07 20:50:56 +0100

    add database dependency

---
+++ geoparser/annotator/db/db.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from appdirs import user_data_dir
+from sqlmodel import Session, SQLModel, create_engine
+
+db_location = Path(user_data_dir("geoparser", "")) / "annotator" / "annotator.db"
+sqlite_url = f"sqlite:///{db_location}"
+
+engine = create_engine(sqlite_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
