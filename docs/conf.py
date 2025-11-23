d038ca0b19521ccd8b86205223fce5d5a8b7c513
Diego Gomes <diego.gomes@uzh.ch>
2025-11-23 11:05:46 +0100

    bump version to 0.3.1 in pyproject.toml and docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "Irchel Geoparser"
 copyright = "2025, Diego Gomes"
 author = "Diego Gomes"
-release = "0.3.0"
+release = "0.3.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
